ca25b3ed9d0b9fecc5152e3fd54316ffcadb8a20
kenna-hwa <76397046+kenna-hwa@users.noreply.github.com>
2021-09-19 22:18:02 +0900

    fill button mode 변경

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,22 @@
+// 변수 선언
+
 const canvas = document.querySelector("#jsCanvas");
 const range = document.querySelector("#jsRange");
 const colors = document.querySelectorAll(".controls_color")
+const mode = document.querySelector("#jsMode")
 const ctx = canvas.getContext("2d");
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
+
+// 조건 선언
 let painting = false;
+let filling = false;
+
+
+//line 그리기
 
 function stopPainting(){
     painting = false;
@@ -47,7 +56,6 @@ colors.forEach(color => color.addEventListener("click", handleColorClick));
 
 // stroke range 변경
 
-
 function handleRangeChange(event){
     const size = event.target.value;
     ctx.lineWidth = size;
@@ -56,4 +64,20 @@ function handleRangeChange(event){
 
 if(range){
     range.addEventListener("input", handleRangeChange)
+}
+
+//fill button
+
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.textContent = "Fill"
+    }else{
+        filling = true;
+        mode.textContent = "Paint"
+    }
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick)
 }
